6b9ff733cd73c23827c79dad9824ead5f6a478eb
patrickriddick <patrick.riddick@curvature.com>
2020-07-15 18:47:49 -0400

    Add project files.

---
+++ WebApiExample/AlphaVantageData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ServiceStack;
+using ServiceStack.Text;
+
+namespace WebApiExample
+{
+    public class AlphaVantageData
+    {
+        public DateTime Timestamp { get; set; }
+        public decimal Open { get; set; }
+
+        public decimal High { get; set; }
+        public decimal Low { get; set; }
+
+        public decimal Close { get; set; }
+        public decimal Volume { get; set; }
+    }
+}
---
+++ WebApiExample/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ServiceStack;
+using ServiceStack.Text;
+
+namespace WebApiExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // retrieve daily prices for MSFT
+            var dailyPrices = $"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MSFT&apikey=7NYTTIF0YYUKXWIQ&datatype=csv"
+                       .GetStringFromUrl().FromCsv<List<AlphaVantageData>>();
+
+
+            var dateCriteria = DateTime.Now.Date.AddDays(-7);
+
+            var sumWeek = (from d in dailyPrices
+                               where d.Timestamp >= DateTime.Now.Date.AddDays(-7)
+                               select d.Volume);
+
+            var averageWeek = sumWeek.Sum()/ sumWeek.Count();
+
+            Console.WriteLine(averageWeek.ToString());
+            Console.ReadLine();
+
+
+
+        }
+    }
+
+
+}
